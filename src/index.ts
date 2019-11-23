e10db99d53522ba132b46c893905dcc71e1f3249
Igor Mucsicska <mucsi_96@yahoo.com>
2019-11-24 00:31:44 +0100

    Initial implementation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
+import { Application } from 'typedoc';
+import { resolve, sep } from 'path';
+
 export function createDocumentation(entry: string): string {
+  const app = new Application({
+    tsConfig: resolve(__dirname, '../tsconfig.json')
+  });
+  const { errors, project } = app.converter.convert([entry]);
+
+  if (errors && errors.length) {
+    errors.map(error => {
+      throw new Error(error.messageText.toString());
+    });
+  }
+
+  const entryModule = Object.values(project.reflections).find(
+    reflection => reflection.originalName.replace(/\//g, sep) === entry
+  );
+
+  if (!entryModule) {
+    throw new Error('Entry module not found');
+  }
+
+  console.log(entryModule);
+
   return entry;
 }
