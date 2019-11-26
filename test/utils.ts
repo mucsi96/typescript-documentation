eae336e18a2a2d61fb919825fedb7d3c22c57db6
Igor Mucsicska <mucsi_96@yahoo.com>
2019-11-26 13:05:39 +0100

    Improve test readbility

--- test/utils.ts
+++ test/utils.ts
@@ -14,10 +14,26 @@ export function cleanup(): void {
   rimraf.sync(testTempDir);
 }
 
-export function testDocumentation(code: { [fileName: string]: string }, markdown: string): void {
-  Object.entries(code).forEach(([fileName, source]) => {
-    writeFileSync(resolve(testTempDir, fileName), source, 'utf8');
-  });
+export function testDocumentation(code: { [fileName: string]: string }): void {
+  Object.entries(code)
+    .filter(([fileName]) => fileName !== 'result')
+    .forEach(([fileName, source]) => {
+      writeFileSync(resolve(testTempDir, fileName), source, 'utf8');
+    });
 
-  expect(createDocumentation(resolve(testTempDir, 'index.ts')).trim()).toEqual(markdown.trim());
+  const output = code.markdown.split('\n');
+  const padding = output.reduce((max, line) => {
+    const match = /^\s*/.exec(line);
+    if (!match || match[0].length < max) {
+      return max;
+    }
+
+    return match[0].length;
+  }, 0);
+  const trimmedOutput = output
+    .map(line => line.substr(padding))
+    .join('\n')
+    .trim();
+
+  expect(createDocumentation(resolve(testTempDir, 'index.ts')).trim()).toEqual(trimmedOutput);
 }
--- test/variable.test.ts
+++ test/variable.test.ts
@@ -2,35 +2,30 @@ import { testDocumentation } from './utils';
 
 describe('variables', () => {
   it('renders exported variables', () => {
-    testDocumentation(
-      {
-        'index.ts': `
-/**
- * Simple variable description
- * line 2
- */
-export const simpleVariable: number = 1;
-        `
-      },
+    testDocumentation({
+      'index.ts': `
+        /**
+         * Simple variable description
+         * line 2
+         */
+        export const simpleVariable: number = 1;
+      `,
+      markdown: `
+        # [simpleVariable](#simple-variable)
+        Simple variable description
+        line 2
+        ##### Type
+        \`number\`
       `
-# [simpleVariable](#simple-variable)
-Simple variable description
-line 2
-##### Type
-\`number\`
-      `
-    );
+    });
   });
 
   it('doesn`t render not exported variables', () => {
-    testDocumentation(
-      {
-        'index.ts': `
-const simpleVariable = 1;
-        `
-      },
-      `
-      `
-    );
+    testDocumentation({
+      'index.ts': `
+        const simpleVariable = 1;
+      `,
+      markdown: ``
+    });
   });
 });
